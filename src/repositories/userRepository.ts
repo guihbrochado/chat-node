737fc8e6ce5c3da236aa5adaacd5e24d657bf380
guihbrochado <guihbrochado@gmail.com>
2025-07-27 16:03:47 -0300

    Adicionando mongosee ao projeto, refatorando repository do user

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,19 +1,23 @@
 import { User } from "../interfaces/user";
+import mongoose, { Schema, model } from "mongoose";
+
+const userSchema = new Schema<User>({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
+const UserModel = model<User>('User', userSchema);
 
 export class UserRepository {
-    private users: User[] = [];
-
     async create(user: Omit<User, 'id' | 'createdAt'>): Promise<User> {
-        const newUser: User = {
-            id: Math.random().toString(36).slice(2),
-            ...user,
-            createdAt: new Date(),
-        };
-        this.users.push(newUser);
-        return newUser;
+        const newUser = new UserModel({ ...user, createdAt: new Date() });
+        await newUser.save();
+        return { id: newUser._id.toString(), ...newUser.toObject() };
     }
 
     async findByUsername(username: string): Promise<User | null> {
-        return this.users.find((user) => user.username === username) || null;
+        const user = await UserModel.findOne({ username });
+        return user ? {id: user._id.toString(), ...user.toObject() } : null;
     }
 }
